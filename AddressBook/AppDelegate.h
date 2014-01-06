4647ffd21265b8036b4dd9c42fba0c9601131d64
Weverton do Couto Timoteo <weverton@wolcan.us>
2014-01-06 15:42:54 -0200

    Initial Commit

---
+++ AddressBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by Weverton do Couto Timoteo on 1/6/14.
+//  Copyright (c) 2014 Weverton do Couto Timoteo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AddressBookTests/AddressBookTests.m
@@ -0,0 +1,34 @@
+//
+//  AddressBookTests.m
+//  AddressBookTests
+//
+//  Created by Weverton do Couto Timoteo on 1/6/14.
+//  Copyright (c) 2014 Weverton do Couto Timoteo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AddressBookTests : XCTestCase
+
+@end
+
+@implementation AddressBookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
